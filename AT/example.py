8b53bb8e5daf2ff58f182f62987bec9d3494c380
NhanNT1312 <nhan.nt1312@gmail.com>
2022-01-12 15:02:03 +0700

    webdriver

---
+++ AT/example.py
@@ -0,0 +1,4 @@
+Machine_PageTitle_Lbl = "//h5[@class='nav-back-text']"
+Machine_CreateWC_Btn = "//button[contains(@class,'btn-new mat-flat-button')]"
+
+Machine_DataTable = "//div[contains(@class,'grid-header-container')]"
